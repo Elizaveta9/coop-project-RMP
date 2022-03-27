8f0a9d07e358eafbdde00b67eecbef1d6a042b62
Elizaveta9 <katkova_lizochka@bk.ru>
2022-03-27 19:13:10 +0300

    Включён Immersive Mode

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -13,6 +13,7 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useCompass = false;
 		config.useAccelerometer = false;
+		config.useImmersiveMode = true;
 		initialize(new Game(), config);
 	}
 }
