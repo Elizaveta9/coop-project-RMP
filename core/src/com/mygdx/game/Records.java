4c5fb8260128e8fd7dd8ecf76bf248b6aed4cfd2
Arseny <terexin-2003@mail.ru>
2022-03-28 19:09:45 +0300

    написан класс для хранения рекорда

---
+++ core/src/com/mygdx/game/Records.java
@@ -0,0 +1,29 @@
+package com.mygdx.game;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Records {
+
+    static private String pathFile = "core/java/com/mygdx/game/record.txt";
+
+    public static void setRecords(int record) throws IOException {
+        File file = new File(pathFile);
+        FileWriter writer = new FileWriter(file);
+        if (record > getRecords()){
+            writer.write(record);
+            writer.flush();
+        }
+
+    }
+
+    public static int getRecords() throws IOException {
+        BufferedReader file = new BufferedReader(new FileReader(new File(pathFile)));
+        int record = Integer.parseInt(file.readLine());
+        return record;
+    }
+}
